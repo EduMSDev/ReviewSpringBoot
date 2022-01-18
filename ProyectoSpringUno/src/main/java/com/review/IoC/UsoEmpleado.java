261ffa5a65063b0ade31515ac82d99a9bbe054f9
Edu <edumartsil95@tutanota.com>
2022-01-18 21:01:50 +0100

    Finished Video 8 Inversion Of Control III

--- ProyectoSpringUno/src/main/java/com/review/IoC/UsoEmpleado.java
+++ ProyectoSpringUno/src/main/java/com/review/IoC/UsoEmpleado.java
@@ -1,16 +1,31 @@
 package com.review.IoC;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class UsoEmpleado {
 
 		public static void main(String[] args) {
 			
-			//Creacion de objeto
+			/*Without the use of beans, it would be necessary to modify 
+			 the code every time you want to change who executes the task.*/
 			Empleados empleado1 = new JefeEmpleado();
 			Empleados empleado2 = new SecretrioEmpleado();
 			Empleados empleado3 = new DirectorEmpleado();
-			
-			//Uso del objeto
+			//Object use
 			System.out.println(empleado1.getTareas());
 			
+			
+			/*With the use of beans, you only need to modify
+			 the xml file if you want to modify who executes the task.*/
+			ClassPathXmlApplicationContext context= new ClassPathXmlApplicationContext("applicationContext.xml");
+			Empleados Juan = context.getBean("miEmpleado", Empleados.class);
+			System.out.println(Juan.getTareas());
+			context.close();
+			
+			/*To change who executes the task, go to the applicattionContext.xml file and change the 
+			beans of  "miEmpleado"to the object you want to execute the task. Example
+
+			bean id="miEmpleado" class="com.review.IoC.Empleados.JefeEmpleado"*/
+
 		}
 }
