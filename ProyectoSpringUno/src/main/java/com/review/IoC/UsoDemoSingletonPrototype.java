b883b77a0ff1ede67085d6fede340947df8bdc0c
Edu <edumartsil95@tutanota.com>
2022-01-24 15:47:29 +0100

    Video 13 Finished Singlenton and Prototype

---
+++ ProyectoSpringUno/src/main/java/com/review/IoC/UsoDemoSingletonPrototype.java
@@ -0,0 +1,24 @@
+package com.review.IoC;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UsoDemoSingletonPrototype {
+	
+	public static void main(String[] args) {
+		
+		//carga de ficheros de configuracion
+		ClassPathXmlApplicationContext context= new ClassPathXmlApplicationContext("applicationContext2.xml");
+		
+		//peticion de beans
+		SecretarioEmpleado maria = context.getBean("miSecretarioEmpleado", SecretarioEmpleado.class);
+		SecretarioEmpleado juan = context.getBean("miSecretarioEmpleado", SecretarioEmpleado.class);
+		System.out.println(maria);
+		System.out.println(juan);
+		
+		if (maria==juan) {
+			System.out.println("Son el mismo objeto");
+		}else {
+			System.out.println("No son el mismo objeto");
+		}
+	}
+}
