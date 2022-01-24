94098eaa3133c8890e71412d177652259fe65153
Edu <edumartsil95@tutanota.com>
2022-01-24 16:25:42 +0100

    Video 15 Finished Java Annotations

---
+++ SpringConAnotaciones/src/test/java/com/review/SpringConAnotaciones/SpringConAnotacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.review.SpringConAnotaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConAnotacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
