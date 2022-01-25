e17a70f6ae1bd6626f7bc0ec895e8490f54c2d73
Edu <edumartsil95@tutanota.com>
2022-01-25 17:52:21 +0100

    Video 32 Finished RequestParam

--- Spring_MVC_ejemplo1/src/com/review/spring/mvc/HolaAlumnoControlador.java
+++ Spring_MVC_ejemplo1/src/com/review/spring/mvc/HolaAlumnoControlador.java
@@ -1,11 +1,9 @@
 package com.review.spring.mvc;
 
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HolaAlumnoControlador {
@@ -21,9 +19,10 @@ public class HolaAlumnoControlador {
 	}
 
 	@RequestMapping("/procesarFormulario2")
-	public String otroProcesoFormulario(HttpServletRequest request, Model modelo) {
+//	public String otroProcesoFormulario(HttpServletRequest request, Model modelo) {
+	public String otroProcesoFormulario(@RequestParam("nombreAlumno")String nombre, Model modelo) {
 		
-		String nombre= request.getParameter("nombreAlumno");
+//		String nombre= request.getParameter("nombreAlumno");
 		nombre+=" es el mejor alumno";
 		String mensajeFinal ="Quien es el mejor alumno?"+nombre;
 		
