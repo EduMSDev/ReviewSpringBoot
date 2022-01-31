ed7701b2cfe6eed47addd35f4577013b5d044b9b
Edu <edumartsil95@tutanota.com>
2022-01-31 21:37:13 +0100

    Video 69 finished CRUD IV DAO

---
+++ GestionCRUD/src/com/review/dao/ClienteDAO.java
@@ -0,0 +1,11 @@
+package com.review.dao;
+
+import java.util.List;
+
+import com.review.controlador.entity.Cliente;
+
+public interface ClienteDAO {
+	
+	public List <Cliente> getClientes();
+
+}
---
+++ GestionCRUD/src/com/review/dao/ClienteDAOClase.java
@@ -0,0 +1,31 @@
+package com.review.dao;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.review.controlador.entity.Cliente;
+
+@Repository
+public class ClienteDAOClase implements ClienteDAO{
+
+	@Override
+	@Transactional
+	public List<Cliente> getClientes() {
+		Session session = sessionFactory.getCurrentSession();
+		Query<Cliente> myQuery = session.createQuery("from Cliente", Cliente.class);
+		List<Cliente> clients =myQuery.getResultList(); 
+		return clients;
+	}
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+
+}
